46ad788ab2413dac4ccc13f94fb5020bb819f84f
DienLe1291 <lvtdien.ag@gmail.com>
2023-05-10 22:04:25 +0700

    Cau hinh cloudinary va multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,16 @@
+import cloudinary from 'cloudinary';
+
+const cloudinaryV2 = cloudinary.v2;
+
+const cloudName = process.env.CLOUDINARY_NAME;
+const apiKey = process.env.CLOUDINARY_API_KEY;
+const apiSecret = process.env.CLOUDINARY_API_SECRET;
+
+cloudinaryV2.config({
+    cloud_name: cloudName,
+    api_key: apiKey,
+    api_secret: apiSecret,
+    secure: true
+})
+
+export default cloudinaryV2;
